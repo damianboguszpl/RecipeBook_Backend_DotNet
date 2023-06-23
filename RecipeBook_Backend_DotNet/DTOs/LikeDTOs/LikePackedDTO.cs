c12d5abfa5b336d96c9fbaf03bbce362c8da6d59
damianboguszpl <damian.bogusz@pollub.edu.pl>
2023-06-23 12:51:42 +0200

    Add LikePackedDTO

---
+++ RecipeBook_Backend_DotNet/DTOs/LikeDTOs/LikePackedDTO.cs
@@ -0,0 +1,11 @@
+﻿using RecipeBook_Backend_DotNet.DTOs.RecipeDTOs;
+using RecipeBook_Backend_DotNet.DTOs.UserDTOs;
+
+namespace RecipeBook_Backend_DotNet.DTOs.LikeDTOs
+{
+    public record struct LikePackedDTO(
+        int Id,
+        UserMinimalDTO User,
+        RecipeMinimalDTO Recipe
+        );
+}
