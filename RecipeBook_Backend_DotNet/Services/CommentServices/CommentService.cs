d31231999b0bae46dcbeedd2cbc8c3e610aace07
damianboguszpl <damian.bogusz@pollub.edu.pl>
2023-06-23 12:50:25 +0200

    Refactor CommentService

--- RecipeBook_Backend_DotNet/Services/CommentServices/CommentService.cs
+++ RecipeBook_Backend_DotNet/Services/CommentServices/CommentService.cs
@@ -1,7 +1,4 @@
-﻿using Azure.Core;
-using Microsoft.EntityFrameworkCore;
-using RecipeBook_Backend_DotNet.DTOs.CommentDTOs;
-using RecipeBook_Backend_DotNet.DTOs.IngredientDTOs;
+﻿using RecipeBook_Backend_DotNet.DTOs.CommentDTOs;
 using RecipeBook_Backend_DotNet.DTOs.RecipeDTOs;
 using RecipeBook_Backend_DotNet.DTOs.UserDTOs;
 
@@ -35,10 +32,6 @@ namespace RecipeBook_Backend_DotNet.Services.CommentServices
             _context.Comments.Add(newComment);
             await _context.SaveChangesAsync();
 
-            var fComment = await _context.Comments.FindAsync(newComment.Id);
-            if (fComment is null)
-                return null;
-
             var userDTO = new UserMinimalDTO
             {
                 Id = user.Id,
@@ -47,8 +40,8 @@ namespace RecipeBook_Backend_DotNet.Services.CommentServices
 
             var commentDTO = new CommentMinimalDTO
             {
-                Id = fComment.Id,
-                Text = fComment.Text,
+                Id = newComment.Id,
+                Text = newComment.Text,
                 User = userDTO
             };
             return commentDTO;
@@ -57,6 +50,7 @@ namespace RecipeBook_Backend_DotNet.Services.CommentServices
         public async Task<CommentMinimalDTO?> DeleteComment(int id)
         {
             var comment = await _context.Comments.FindAsync(id);
+
             if (comment is null)
                 return null;
             else
@@ -64,7 +58,9 @@ namespace RecipeBook_Backend_DotNet.Services.CommentServices
                 _context.Comments.Remove(comment);
                 await _context.SaveChangesAsync();
             }
+
             var user = await _context.Users.FindAsync(comment.UserId);
+
             if (user is null)
                 return null;
 
@@ -85,33 +81,28 @@ namespace RecipeBook_Backend_DotNet.Services.CommentServices
 
         public async Task<List<CommentPackedDTO>?> GetAllCommentsByRecipe(int id)
         {
-            var recipe = await _context.Recipes.FindAsync(id);
+            var recipe = await _context.Recipes
+                .Include(c => c.User)
+                .Include(c => c.Comments)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
             if (recipe is null)
                 return null;
 
-            var user = await _context.Users.FindAsync(recipe.UserId);
-            if (user is null)
-                return null;
-
             RecipeMinimalDTO recipeDTO = new() { Id = id };
+
             UserMinimalDTO userDTO = new(){ 
-                Id = user.Id, 
-                Username = user.Username
+                Id = recipe.User.Id, 
+                Username = recipe.User.Username
             };
 
-            var comments = await _context.Comments.
-                Where(comment => comment.RecipeId == id).
-                ToListAsync();
-            if (comments is null)
-                return null;
-
-            List<CommentPackedDTO> commentsDTO = comments.Select(comment => new CommentPackedDTO
+            List<CommentPackedDTO> commentsDTO = recipe.Comments?.Select(comment => new CommentPackedDTO
             {
                 Id = comment.Id,
                 Text = comment.Text,
                 User = userDTO,
                 Recipe = recipeDTO
-            }).ToList();
+            }).ToList() ?? new List<CommentPackedDTO>();
 
             return commentsDTO;
         }
