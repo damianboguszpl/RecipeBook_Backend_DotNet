e316ea0b68c71b915213763bf3356f79822164ae
damianboguszpl <damian.bogusz@pollub.edu.pl>
2023-06-23 18:41:13 +0200

    Update User Model

--- RecipeBook_Backend_DotNet/Models/User.cs
+++ RecipeBook_Backend_DotNet/Models/User.cs
@@ -7,7 +7,7 @@ namespace RecipeBook_Backend_DotNet.Models
         public int Id { get; set; }
         public string PermissionLevel { get; set; } = String.Empty;
         public string Username { get; set; } = String.Empty;
-        public string Password { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
 
         [JsonIgnore]
         public List<Recipe>? Recipes { get; set; }
