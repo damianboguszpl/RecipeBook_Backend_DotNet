5c0ff1f9e1b12a7716369c2bb6490eb42fdfc600
damianboguszpl <damian.bogusz@pollub.edu.pl>
2023-06-23 12:50:42 +0200

    Refactor IngredientService

--- RecipeBook_Backend_DotNet/Services/IngredientServices/IngredientService.cs
+++ RecipeBook_Backend_DotNet/Services/IngredientServices/IngredientService.cs
@@ -15,6 +15,7 @@ namespace RecipeBook_Backend_DotNet.Services.IngredientServices
         public async Task<IngredientMinimalDTO?> AddIngredient(IngredientCreateDTO request)
         {
             var recipe = await _context.Recipes.FindAsync(request.RecipeId);
+
             if (recipe is null)
                 return null;
 
@@ -26,17 +27,14 @@ namespace RecipeBook_Backend_DotNet.Services.IngredientServices
             _context.Ingredients.Add(newIngredient);
             await _context.SaveChangesAsync();
 
-            var fIngredient = await _context.Ingredients.FindAsync(newIngredient.Id);
-            if (fIngredient is null)
-                return null;
-
-            var ingredientDTO = new IngredientMinimalDTO { Id = fIngredient.Id, Name = fIngredient.Name };
+            var ingredientDTO = new IngredientMinimalDTO { Id = newIngredient.Id, Name = newIngredient.Name };
             return ingredientDTO;
         }
 
         public async Task<IngredientMinimalDTO?> DeleteIngredient(int id)
         {
             var ingredient = await _context.Ingredients.FindAsync(id);
+
             if (ingredient is null)
                 return null;
             else
@@ -52,6 +50,7 @@ namespace RecipeBook_Backend_DotNet.Services.IngredientServices
         public async Task<List<IngredientPackedDTO>?> GetAllIngredients()
         {
             var ingredients = await _context.Ingredients.ToListAsync();
+
             if (ingredients is null)
                 return null;
 
@@ -67,23 +66,21 @@ namespace RecipeBook_Backend_DotNet.Services.IngredientServices
 
         public async Task<List<IngredientPackedDTO>?> GetAllIngredientsByRecipe(int id)
         {
-            var recipe = await _context.Recipes.FindAsync(id);
+            var recipe = await _context.Recipes
+                .Include(c => c.Ingredients)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
             if (recipe is null)
                 return null;
+
             RecipeMinimalDTO recipeDTO = new() { Id = id };
 
-            var ingredients = await _context.Ingredients.
-                Where(recipe => recipe.RecipeID == id).
-                ToListAsync();
-            if (ingredients is null)
-                return null;
-
-            List<IngredientPackedDTO> ingredientsDTO = ingredients.Select(ingredient => new IngredientPackedDTO
+            List<IngredientPackedDTO> ingredientsDTO = recipe.Ingredients?.Select(ingredient => new IngredientPackedDTO
             {
                 Id = ingredient.Id,
                 Name = ingredient.Name,
                 Recipe = recipeDTO
-            }).ToList();
+            }).ToList() ?? new List<IngredientPackedDTO>();
 
             return ingredientsDTO;
         }
@@ -91,8 +88,10 @@ namespace RecipeBook_Backend_DotNet.Services.IngredientServices
         public async Task<IngredientPackedDTO?> GetIngredient(int id)
         {
             var ingredient = await _context.Ingredients.FindAsync(id);
+
             if (ingredient is null)
                 return null;
+
             return new IngredientPackedDTO {
                 Id = ingredient.Id,
                 Name = ingredient.Name,
