4e071552c2a24305ea87d514211055d13a9792c9
damianboguszpl <damian.bogusz@pollub.edu.pl>
2023-06-24 19:45:11 +0200

    Add DTO

---
+++ RecipeBook_Backend_DotNet/DTOs/RefreshTokenDTOs/RefreshTokenMinimalDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeBook_Backend_DotNet.DTOs.RefreshTokenDTOs
+{
+    public record struct RefreshTokenMinimalDTO(
+        int Id,
+        string Token,
+        DateTime Created,
+        DateTime Expires
+        );
+}
