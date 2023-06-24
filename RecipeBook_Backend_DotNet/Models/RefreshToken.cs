85ce655ab1d9b7d4e364ebcf8398f0a3002da13c
damianboguszpl <damian.bogusz@pollub.edu.pl>
2023-06-24 19:43:28 +0200

    Add RefreshToken Model

---
+++ RecipeBook_Backend_DotNet/Models/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeBook_Backend_DotNet.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+
+        //public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
